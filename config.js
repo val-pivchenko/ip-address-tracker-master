57b5366d45b32a15829e2583163f7088ee8d1419
Val <91499731+val-pivchenko@users.noreply.github.com>
2021-12-17 12:57:40 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const config = {
     IPIFY_API_KEY: 'at_hgtpBMeTp2unoT4MAosVSHkTHvK3c',
-    MAP_ACCESS_TOKEN: 'pk.eyJ1IjoidmFsLXBpdmNoZW5rbyIsImEiOiJja3hhb3RsMGgwMG53Mm5wejN4a3JtNDczIn0.fht01we9uf30JgKTNRt87w'
+    MAP_ACCESS_TOKEN: 'pk.eyJ1IjoidmFsLXBpdmNoZW5rbyIsImEiOiJja3gwcDFzbGIwc2hhMm9tcjRhZ3Fma3djIn0.zLUZcgsTC2tGhEnkbNhWBA'
 }
